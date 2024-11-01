4ecead99be6a4b0dbf4270d310634133744e4d54
Guilherme Cavalcanti <144628123+guilhermisso@users.noreply.github.com>
2024-11-01 00:31:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function votar() {
+    // Obter o valor selecionado (opcional para usar o valor depois)
+    const candidato = document.getElementById("candidato").value;
+
+    // Redirecionar para a página de confirmação
+    window.location.href = "fim.html";
+}
